3d614ac030ea87a8ffcd219083165468eaea139a
AlesciDavide <davi.alesci@Gmail.com>
2024-07-07 11:31:33 +0200

    scaffoldin per componenti

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import Header from './components/Header.vue'
+
+export default{
+  components:{
+    Header,
+  },
+    data() {
+        return{
+            
+        }
+    }
+}
+</script>
+
+<template>
+  <Header />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<script>
+export default{
+    data() {
+        return{
+            
+        }
+    }
+}
+</script>
+
+<template>
+<h1>Sistemazione componenti</h1>
+</template>
+
+<style scoped>
+
+</style>
