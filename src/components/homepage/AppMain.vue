7d96e5ebe43b4ea6ec1a49f7ca51e5eecfbfd991
AlesciDavide <davi.alesci@gmail.com>
2024-10-28 16:32:06 +0100

    fix

--- src/components/homepage/AppMain.vue
+++ src/components/homepage/AppMain.vue
@@ -158,7 +158,7 @@ export default {
         justify-content: center;
         align-items: center;
         position: relative;
-        /* margin-top: 54px; */
+        margin-top: 54px;
 
         div {
             padding: 0 30px;
